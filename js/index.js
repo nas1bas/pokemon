3e9e4886ddc0fb3a842c72904c8c81785f6ec23a
nas1bas <nasiba4647@gmail.com>
2022-01-10 20:09:21 +0300

    add files and folders

---
+++ js/index.js
@@ -0,0 +1,223 @@
+var elFilmsList = document.querySelector(".films-wrapper")
+
+for (var i = 0; i < films.lenght; i++) {
+    var item = document.createElement("li")
+
+    console.log(item);
+    console.log(films[i]);
+}
+
+// var elText = document.querySelector(".text")
+// elText.textContent = "kndckj"
+
+// var a = 5
+//     b = 5
+// var c = (a+b)
+// console.log(c)
+
+// var ves = 73
+// var boy = 1.7
+// var bmi = ( ves / (boy*boy))
+// if (bmi>25) {
+//     elText.textContent = "semiz"
+// }
+// else if (bmi<20 && bmi>25) {
+//     elText.textContent = "norm"
+// }
+// else {
+//     elText.textContent = "ozgin"
+// }
+
+// var colors = ["ok", "qizil", 'sariq']
+// console.log(colors)
+// colors[2] = "qora"
+// colors.push("moviy")
+// colors.push(4)
+// console.log (colors.length)
+
+// function hi (myLove, name) {
+//  elText.textContent=("I love you," + myLove +" "+ name)
+// }
+// hi("Alisher", "topolonchi")
+// hi("Aju", "uyqichi")
+ 
+// function hisobla (kavdratini) {
+//    return kavdratini*kavdratini
+// }
+// elText.textContent = hisobla(3)
+
+
+// var w = 2
+// var z =1
+// console.log(w+z)
+// console.log(w-z)
+// console.log(w/z)
+// console.log(w*z)
+// console.log(w%z)
+// console.log(++w)
+// console.log(w++)
+// console.log(--z)
+// console.log(z--)
+// console.log(w===z)
+// console.log(w!==z)
+// console.log(w>z)
+// console.log(w<z)
+
+// var x = 35
+// if (x >= 25) {
+//  console.log("25% skidka")
+// } else {
+//     console.log("5$ skidka")
+// }
+
+// let mijoz = 5 ;
+// let tekshirish =  mijoz == 20 ? 'azo' :'azo emas';
+// console.log('mijoz do`konga ', tekshirish);
+
+// var isOpen = false 
+// console.log(!!isOpen)
+
+// var a
+// var b = null
+// var c = undefined
+// var d = 4
+// var e = "five"
+// var f = a || b || c || d || e 
+// console.log (f)
+
+// var color1 = "oq"
+// var color2 = "qora"
+// var color = color1
+// color1 = color2
+// color2 = color
+// console.log(color1)
+// console.log(color2)
+
+// var hour = 19
+// if (hour >= 6 && hour < 12) {
+//     console.log("Tong")
+// } else if (hour > 12 && hour < 18) {
+//     console.log("Tushlik")
+// }else {
+//     console.log("Kech")
+// }
+
+// for (var i=0; i<10; i+2) {
+//     console.log(i)
+// }
+
+// var array = ["olma", "anor", "nok", "qulupnay"]
+// console.log(array.length)
+// for (var i=0; i<array.length; i++){
+//     elText.textContent = array
+// }
+// elText.textContent = array[0]forga
+
+// function forga (array) {
+//     for (var i=0; i<array.length; i++){
+//         elText.textContent = (array[i])
+//     }
+// }
+//  console.log(forga(["olma", "anor", "nok", "qulupnay" ]))
+
+// var array = ["olma", "anor", "nok", "qulupnay"]
+// var elBody = document.querySelector(".body")
+// var elList = document.createElement("ul")
+// function liYasash (liqosh) {
+//     var elLi = document.createElement("li")
+//     elLi.textContent = liqosh
+//     elList.appendChild(elLi)
+//     elBody.appendChild(elList)
+// }
+// for (var i = 0; i < array.length; i++) {
+//     liYasash(array[i])
+// }
+// liYasash("limon")
+
+// var array = ["olma", "anor", "nok", "qulupnay"]
+// for (var i = 0; i<array.length; i++){
+//     var li = document.createElement("li")
+//     li.textContent = array[i]
+    // elBody.appendChild(li)
+// }
+// array.push("kivi") SAVOL
+
+// function longWord (text) {
+//     var split = text.split(" ")
+// var nat = split[0]
+// for (var i=1; i<split.length; i++) {
+//     if (nat.length < split[i].length) {
+//         nat = split[i]
+//     }
+// }
+// return nat
+// }
+// var result = longWord("olma", "anor", "nok", "qulupnay")
+// console.log(result) SAVOL
+
+// var object = {
+//     name: "alish",
+//     age: 13,
+//     class: 6,
+//     salom: function () {
+//         console.log("HI")
+//     }
+// }
+// object.salom()
+//  elText.textContent = object.name
+
+// var sana = new Date().getMonth()
+// console.log(sana)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
